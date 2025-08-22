0dfaf6e4787102aa857ecd72f2f11b38b7abf323
Mazen6999 <114710326+Mazen6999@users.noreply.github.com>
2025-08-22 12:54:38 +0000

    edit test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,17 @@
 show tables;
+
+DROP TABLE IF EXISTS students;
+
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    age INT check (age >= 18),
+    grade INT NOT NULL,
+    exam_date DATE DEFAULT (current_date)
+);
+
+INSERT INTO students (name, age, grade) VALUES ('Alice', 20, 90);
+
+SELECT * FROM students;
