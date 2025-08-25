c9cb04f631ee5245c82b204b4086a692d64a1620
Mazen6999 <mazen.shams6999@gmail.com>
2025-08-25 20:13:20 +0300

    Update sql procedures.sql

--- sql procedures.sql
+++ sql procedures.sql
@@ -1,13 +1,17 @@
 DROP PROCEDURE IF EXISTS hello_world;
 
-
 CREATE PROCEDURE hello_world()
 BEGIN
   SELECT 'hello_world';
   SELECT 'this is test';
 END
 
+
+
 CALL hello_world();
+
+
+DROP PROCEDURE IF EXISTS hello_world2;
 
 CREATE PROCEDURE hello_world2(IN name VARCHAR(20))
 BEGIN
@@ -16,5 +20,6 @@ BEGIN
 END
 
 
-
 CALL hello_world2('Alice');
+CALL hello_world2('Bob');
+
